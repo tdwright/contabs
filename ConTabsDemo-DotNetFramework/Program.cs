4d6cd33f79e252bb35dc0189db7bbfc6896b54b5
tdwright <tom@tdwright.co.uk>
2017-11-21 19:54:17 +0000

    Added output to the .NET Framework demo

--- ConTabsDemo-DotNetFramework/Program.cs
+++ ConTabsDemo-DotNetFramework/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ConTabs;
 
 namespace ConTabsDemo_DotNetFramework
 {
@@ -8,8 +10,24 @@ namespace ConTabsDemo_DotNetFramework
         {
             Console.WriteLine("CONTABS .NET FRAMEWORK DEMO");
 
+            var Data = new List<TestClass>
+            {
+                new TestClass{StringCol="Cats", IntCol = 2},
+                new TestClass{StringCol="Dogs", IntCol = 1},
+                new TestClass{StringCol="Chickens", IntCol = 3}
+            };
+
+            var table = Table<TestClass>.Create(Data);
+            Console.WriteLine(table.ToString());
+
             Console.WriteLine("Press return to exit...");
             Console.ReadLine();
+        }
+
+        private class TestClass
+        {
+            public string StringCol { get; set; }
+            public int IntCol { get; set; }
         }
     }
 }
