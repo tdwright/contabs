a3ce1e66b2f2bf47e75be893f095d99acee1bce9
tdwright <tom@tdwright.co.uk>
2017-12-18 19:02:55 +0000

    Added some tests to cover usage - particularly the order of operations

---
+++ ConTabs.Tests/UsageTests.cs
@@ -0,0 +1,42 @@
+﻿using ConTabs.TestData;
+using NUnit.Framework;
+using Shouldly;
+using System;
+
+namespace ConTabs.Tests
+{
+    [TestFixture]
+    class UsageTests
+    {
+        [Test]
+        public void OnceFormatSet_FormatCanBeUpdated()
+        {
+            // Arrange
+            var tableObj = Table<TestDataType>.Create();
+            tableObj.TableStyle = Style.Default;
+
+            // Act
+            tableObj.TableStyle = Style.Heavy;
+            var result = tableObj.ToString();
+
+            // Assert
+            result[0].ShouldBe('#');
+        }
+
+        [Test]
+        public void OnceFormatSet_CornerCanBeUpdated()
+        {
+            // Arrange
+            var tableObj = Table<TestDataType>.Create();
+            tableObj.TableStyle = Style.Default;
+
+            // Act
+            tableObj.TableStyle.Corners.CornerTopLeft = '$';
+            var result = tableObj.ToString();
+
+            // Assert
+            result[0].ShouldBe('$');
+        }
+
+    }
+}
