602d4dec14ac78d213f89a8f16536f8d8b9cb151
Travis Nesbit <tnesbit450@gmail.com>
2017-12-18 22:56:35 -0800

    Bugfix #13. Table<T>.Create(data) throws exception when data model does not have public properties.

--- ConTabs.Tests/Table-Basic.cs
+++ ConTabs.Tests/Table-Basic.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Shouldly;
 using System.Collections.Generic;
+using ConTabs.Exceptions;
 using ConTabs.TestData;
 
 namespace ConTabs.Tests
@@ -44,6 +45,26 @@ namespace ConTabs.Tests
             table.Columns.Count.ShouldBe(4);
         }
 
+        [Test]
+        public void Table_GivenClassWithoutPublicProperties_ThrowsPublicPropertiesNotFoundException()
+        {
+            // Arrange
+            var listOfTestClasses = DataProvider.ListOfInvalidTestData();
+
+            // Act - using try-catch due to static builder pattern, which precludes use of Assert.Throws.
+            var message = string.Empty;
+            try
+            {
+                var table = Table<InvalidTestDataType>.Create(listOfTestClasses);
+            }
+            catch (PublicPropertiesNotFoundException e)
+            {
+                message = e.Message;
+            }
+            
+            // Assert
+            Assert.AreEqual(message, "On Table<T> creation, no valid properties were identified. Check access modifiers.");
+        }
     }
 
     
--- ConTabs/Table.cs
+++ ConTabs/Table.cs
@@ -1,64 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using ConTabs.Exceptions;
 
 namespace ConTabs
 {
     public class Table<T> where T:class
     {
-        public List<Column> Columns { get; set; }
-        internal List<Column> _colsShown => Columns.Where(c => !c.Hide).ToList();
-        public Style TableStyle { get; set; }
+        private Table()
+        {
+            TableStyle = Style.Default;
+            Columns = GetPublicPropertiesForColumns();
+            
+        }
 
-        private IEnumerable<T> _data;
+        private static List<Column> GetPublicPropertiesForColumns()
+        {
+            var tableProperties = typeof(T).GetTypeInfo().DeclaredProperties;
+            var propertyList = tableProperties
+                .Where(p => p.GetMethod.IsPublic)
+                .Select(p => new Column(p.PropertyType, p.Name))
+                .ToList();
+            
+            if (!propertyList.Any()) throw new PublicPropertiesNotFoundException();
+            
+            return propertyList;
+        }
+
+        public List<Column> Columns { get; }
+        public Style TableStyle { get; set; }
+        
+        public static Table<T> Create()
+        {
+            return new Table<T>();
+        }
+        
+        public static Table<T> Create(IEnumerable<T> source)
+        {
+            return new Table<T>()
+            {
+                Data = source
+            };
+        }
+        
+        public override string ToString() => OutputBuilder<T>.BuildOutput(this, TableStyle);
+        
         public IEnumerable<T> Data
         {
-            get
-            {
-                return _data;
-            }
-            set
+            get => _data;
+            private set
             {
                 _data = value;
                 foreach (var col in Columns)
                 {
                     col.Values = _data.Select(d =>
                     {
-                        Type t = typeof(T);
-                        PropertyInfo p = t.GetRuntimeProperty(col.PropertyName);
+                        var t = typeof(T);
+                        var p = t.GetRuntimeProperty(col.PropertyName);
                         return p.GetValue(d);
                     }).ToList();
                 }
             }
         }
-
-        public static Table<T> Create()
-        {
-            return new Table<T>();
-        }
-
-        public static Table<T> Create(IEnumerable<T> Source)
-        {
-            return new Table<T>()
-            {
-                Data = Source
-            };
-        }
-
-        private Table()
-        {
-            TableStyle = Style.Default;
-            var props = typeof(T).GetTypeInfo().DeclaredProperties;
-            Columns = props
-                .Where(p => p.GetMethod.IsPublic)
-                .Select(p => new Column(p.PropertyType, p.Name))
-                .ToList();
-        }
-
-        public override string ToString()
-        {
-            return OutputBuilder<T>.BuildOutput(this, TableStyle);
-        }
+        
+        private IEnumerable<T> _data;
+        internal List<Column> _colsShown => Columns.Where(c => !c.Hide).ToList();
     }
 }
