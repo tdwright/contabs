cae37218610c108036eedef463c601924822936d
tdwright <tom@tdwright.co.uk>
2018-11-21 22:51:06 +1100

    Demo program to show the attribute system in action

---
+++ ConTabsDemo-Attributes/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConTabs;
+using System;
+
+namespace ConTabsDemo_Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CONTABS ATTRIBUTE DEMO");
+
+            // Get some data (can be an IEnumerable of anything)
+            var Data = DemoDataProvider.ListOfDemoData();
+
+            // Write it to the console (in one line!)
+            Console.WriteLine(Table.Create(Data));
+
+            Console.WriteLine("Press return to exit...");
+            Console.ReadLine();
+        }
+    }
+}
